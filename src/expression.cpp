15d94259301e8fc63125d62f5a82a8f3cf6e4843
Lukasz Maciejewski <lukasz.m.maciejewski@gmail.com>
2018-07-10 17:27:57 +0200

    cleaning up some

--- src/expression.cpp
+++ src/expression.cpp
@@ -4,8 +4,8 @@
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 =============================================================================*/
-#include "../fun/expression_def.hpp"
-#include "../fun/config.hpp"
+#include "expression_def.hpp"
+#include "config.hpp"
 
 // EXPRESSION_INSTANTIATE_VISIT_BEGIN
 namespace fun { namespace parser
--- src/printer.cpp
+++ src/printer.cpp
@@ -4,7 +4,7 @@
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 =============================================================================*/
-#include "../fun/printer.hpp"
+#include "printer.hpp"
 #include <boost/foreach.hpp>
 #include <boost/variant/apply_visitor.hpp>
 #include <boost/assert.hpp>
